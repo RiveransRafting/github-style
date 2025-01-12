18edc2d0fc62f5e717b0348c78d7fc409460c0c4
RiveransRafting <gurfin@gurfin.se>
2025-01-12 16:34:37 +0100

    wait for page load

--- static/js/global.js
+++ static/js/global.js
@@ -1,34 +1,38 @@
-window.onscroll = function (e) {
-    const headerImg = document.querySelector('#headerImg');
-    const headerStuck = document.querySelector('#headerStuck');
-    if (headerImg.getBoundingClientRect().bottom <= 0) {
-        headerStuck.classList.add('is-stuck');
-        if (window.innerWidth >= 1280) {
-            headerStuck.setAttribute('style', 'top: 12px;')
+window.addEventListener('load', function () {
+    
+    window.onscroll = function (e) {
+        const headerImg = document.querySelector('#headerImg');
+        const headerStuck = document.querySelector('#headerStuck');
+        if (headerImg.getBoundingClientRect().bottom <= 0) {
+            headerStuck.classList.add('is-stuck');
+            if (window.innerWidth >= 1280) {
+                headerStuck.setAttribute('style', 'top: 12px;')
+            } else {
+                headerStuck.setAttribute('style', 'top: 0;')
+            }
         } else {
-            headerStuck.setAttribute('style', 'top: 0;')
+            headerStuck.classList.remove('is-stuck');
         }
-    } else {
-        headerStuck.classList.remove('is-stuck');
+    };
+    
+    document.getElementById("switch_theme_button").addEventListener("click", function () {
+        switchTheme();
+    });
+    
+    var style = localStorage.getItem('data-color-mode')
+    githubIconElement = document.getElementById('github-icon')
+    twitterIconElement = document.getElementById('twitter-icon')
+    if (style == 'light') {
+        if (githubIconElement) githubIconElement.setAttribute('fill', '#24292e')
+        if (twitterIconElement) twitterIconElement.setAttribute('fill', 'black')
     }
-};
+    else {
+        if (githubIconElement) {
+            githubIconElement.removeAttribute('fill')
+            githubIconElement.setAttribute('class', 'octicon')
+            githubIconElement.setAttribute('color', '#f0f6fc')
+        }
+        if (twitterIconElement) twitterIconElement.setAttribute('fill', 'white')
+    }
 
-document.getElementById("switch_theme_button").addEventListener("click", function () {
-    switchTheme();
 });
-
-var style = localStorage.getItem('data-color-mode')
-githubIconElement = document.getElementById('github-icon')
-twitterIconElement = document.getElementById('twitter-icon')
-if (style == 'light') {
-    if (githubIconElement) githubIconElement.setAttribute('fill', '#24292e')
-    if (twitterIconElement) twitterIconElement.setAttribute('fill', 'black')
-}
-else {
-    if (githubIconElement) {
-        githubIconElement.removeAttribute('fill')
-        githubIconElement.setAttribute('class', 'octicon')
-        githubIconElement.setAttribute('color', '#f0f6fc')
-    }
-    if (twitterIconElement) twitterIconElement.setAttribute('fill', 'white')
-}
